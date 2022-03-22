c58ba9cc193a4b8ef4f6f5a66dc1d163a42e9abb
fcolakovic2 <fcolakovic2@etf.unsa.ba>
2022-03-22 18:42:39 +0100

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Faris Čolaković on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
